9aa3dc1bb9ffb8f84ba3c7f14250c44e6a82ff0e
Martin Meredith <martin.meredith@hystreet.co.uk>
2018-09-26 10:04:48 +0100

    Basic light switch app

--- src/main.js
+++ src/main.js
@@ -1,32 +1,45 @@
-const { app, BrowserWindow } = require("electron");
-const luxafor = require("./utils/luxafor");
+const { app, Menu, Tray, BrowserWindow } = require('electron');
+const luxafor = require('./utils/luxafor');
 
-let mainWindow;
+let tray;
 
-const createWindow = () => {
-  mainWindow = new BrowserWindow({ width: 800, height: 600 });
-
-  mainWindow.loadFile(`${__dirname}/index.html`);
-
-  mainWindow.on("closed", function() {
-    luxafor.off();
-
-    mainWindow = null;
-  });
+const modes = {
+  off: {
+    label: 'Off',
+    icon: `${__dirname}/icons/switch.png`,
+    click: () => luxafor.fadeTo('#000')
+  },
+  green: {
+    label: 'Ok',
+    icon: `${__dirname}/icons/checkmark.png`,
+    click: () => luxafor.fadeTo('#0f0')
+  },
+  red: {
+    label: 'Busy',
+    icon: `${__dirname}/icons/cross.png`,
+    click: () => luxafor.fadeTo('#f00')
+  }
 };
 
-app.on("ready", createWindow);
-
-app.on("window-all-closed", function() {
-  luxafor.off();
-
-  if (process.platform !== "darwin") {
-    app.quit();
+const ready = () => {
+  if (process.platform === 'darwin') {
+    app.dock.hide();
   }
-});
 
-app.on("activate", function() {
-  if (mainWindow === null) {
-    createWindow();
+  tray = new Tray(modes.off.icon);
+  const contextMenu = Menu.buildFromTemplate([
+    ...Object.values(modes),
+    { label: 'Quit', click: () => app.quit() }
+  ]);
+
+  tray.setToolTip('Lightswitch');
+  tray.setContextMenu(contextMenu);
+};
+
+app.on('ready', ready);
+
+app.on('activate', function() {
+  if (tray === null) {
+    ready();
   }
 });
